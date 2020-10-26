53331c5087fac2d2784391d3f7c240f1b7c7b9f2
Henrique Fittipaldi <henrique.gfitti@gmail.com>
2020-10-25 22:34:58 -0300

    route_criar

--- jogoteca.py
+++ jogoteca.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,6 +10,10 @@ class Jogo():
         self.console = console
 
 
+jogo1 = Jogo('Zelda: Ocarina of Time', 'Aventura', 'Nintendo64')
+jogo2 = Jogo('Zelda: Majora´s Mask', 'Aventura', 'Nintendo64')
+jogos_lista = [jogo1, jogo2]
+
 @app.route('/')
 def ola():
     return '<h1>Olá Flask</h1>'
@@ -17,15 +21,22 @@ def ola():
 
 @app.route('/lista')
 def lista():
-    jogo1 = Jogo('Zelda: Ocarina of Time', 'Aventura', 'Nintendo64')
-    jogo2 = Jogo('Zelda: Majora´s Mask', 'Aventura', 'Nintendo64')
-    jogos_lista = [jogo1, jogo2]
     return render_template('lista.html', titulo='Jogos', jogos=jogos_lista)
 
 
 @app.route('/novo')
 def novo():
     return render_template('novo.html', titulo='Novo jogo')
+
+
+@app.route('/criar')
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    jogos_lista.append(jogo)
+    return render_template('lista.html', titulo='Jogos', jogos=jogos_lista)
 
 
 app.run()
